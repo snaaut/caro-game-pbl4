81e5a475ba529152ad65a797cc85b0ce410250af
tuan <llddtt1102@gmail.com>
2023-08-26 12:04:46 +0700

    helloo

---
+++ caro-game-client-main/src1/controller/CaroActionListener.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.Point;
+
+public class CaroActionListener implements ActionListener{
+	
+	private Point point;
+	
+	public CaroActionListener(Point p) {
+		// TODO Auto-generated constructor stub
+		point = p;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
